b9061aa90d911e45043cf19348b83e49588d5432
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-10-25 11:38:18 -0600

    Ignora dependencias y carpetas generadas automáticamente

--- .next/dev/server/app/api/credit-score/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/credit-score/route.js")
-R.c("server/chunks/node_modules_next_550523ba._.js")
-R.c("server/chunks/[root-of-the-server]__f0333f97._.js")
-R.c("server/chunks/_next-internal_server_app_api_credit-score_route_actions_a5b5582e.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/credit-score/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/credit-score/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
