aac5a90107caca02905892ab969f378e518f392f
onyxdude <leocerva29@gmail.com>
2025-10-25 13:26:07 -0600

    [CHORES] cleanup

--- agent/main.py
+++ agent/main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from api_routers import api_router
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from routers import agent_router
 import uvicorn
 from dotenv import load_dotenv
